c9ef0ed37a1cdaf997bb59eb4073f86615526fba
kevkoch <50231661+kevkoch@users.noreply.github.com>
2020-10-17 18:09:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Beethoven",
   dependencies: [
-    .Package(url: "https://github.com/kevkoch/Pitchy", majorVersion: 2),
+    .Package(url: "https://github.com/kevkoch/Pitchy.git", majorVersion: 2),
   ]
 )
